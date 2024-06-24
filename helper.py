2cb6aea9a0bc32e3081f83fc96048c811b6023fd
debdeepmitra <106628167+debdeepmitra@users.noreply.github.com>
2024-06-24 19:42:54 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,8 +2,8 @@ import joblib
 import numpy as np
 import pandas as pd
 
-encoder1 = joblib.load(r'Model\author_encoder.joblib')
-encoder2 = joblib.load(r'Model\geometry_encoder.joblib')
+encoder1 = joblib.load(r'Model/author_encoder.joblib')
+encoder2 = joblib.load(r'Model/geometry_encoder.joblib')
 
 def author_encoder(input_value):
     return encoder1.transform([input_value])[0]
